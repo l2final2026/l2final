a6cba740f5c47b153c80c3d5da0b555c2d86e80e
l2final2026 <joanasantos791@hotmail.com>
2026-03-23 19:00:55 -0300

    Delete banco.sql

--- banco.sql
+++
@@ -1,4 +0,0 @@
-alter table accounts add email varchar(255);
-alter table accounts add pergunta varchar(255);
-alter table accounts add resposta varchar(255);
-alter table accounts add anotacao varchar(255);
